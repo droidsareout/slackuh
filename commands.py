ae482d94f4f1d978733cc4fe39ebe7ee32d3817b
droid <droidsareut@proton.me>
2025-12-11 19:42:00 -0500

    Add commands file and help function within

--- commands.py
+++ commands.py
@@ -1,1 +1,13 @@
+def list_commands():
+    commands = {
+        ".exit": "Exit this interface",
+        ".help": "View this program's available commands.",
+        ".add": "Add a question and its answer. [question], [answer]",
+        ".delete": "Remove a question and its answer. [question number]",
+        ".clear": "Clear questions and answers.",
+        ".list": "View questions and their answers.",
+    }
 
+    print ("======== COMMANDS ========")
+    for command, info in commands.items():
+        print(f"{command}: {info}")
