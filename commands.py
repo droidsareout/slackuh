132b8e77f8781bd29df8da74390833293a1404bb
droidsareut <droidsareut@proton.me>
2025-12-24 13:36:30 -0500

    Add list_commands function

--- commands.py
+++ commands.py
@@ -1,1 +1,9 @@
-
+def list_commands():
+    commands = {
+        "exit": "Close the Slackuh interface.",
+        "help": "View Slackuh's commands.",
+        "add": "Add a question and its answer. [question] [answer]",
+        "delete": "Remove a question and its answer. [question #]",
+        "clear": "Clear all question and answers.",
+        "list": "View all questions and their answers.",
+    }
