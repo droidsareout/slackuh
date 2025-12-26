cd02e8269a872ce92799e86f64c066dda5481845
droidsareut <droidsareut@proton.me>
2025-12-26 16:21:29 -0500

    Change variable for text sent to client (I forgot we don't do camel casing)

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ running_program = True
 print("Slackuh \nType \".help\" for a list of commands.")
 
 while running_program:
-    sentText = input(">> ")
+    sent_text = input(">> ")
 
-    match sentText:
+    match sent_text:
         case ".exit":
             running_program = False
         case ".list":
