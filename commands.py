4a2f8d1d7b61f0ddab1b1da82240ef5638fa44d6
droidsareut <droidsareut@proton.me>
2025-12-26 16:35:44 -0500

    Move commands dictionary outside list_commands function to be global

--- commands.py
+++ commands.py
@@ -1,15 +1,14 @@
 question_answers = {}
+commands = {
+    "exit": "Close the Slackuh interface.",
+    "help": "View Slackuh's commands.",
+    "add": "Add a question and its answer. [question] [answer]",
+    "delete": "Remove a question and its answer. [question #]",
+    "clear": "Clear all question and answers.",
+    "list": "View all questions and their answers.",
+}
 
 def list_commands():
-    commands = {
-        "exit": "Close the Slackuh interface.",
-        "help": "View Slackuh's commands.",
-        "add": "Add a question and its answer. [question] [answer]",
-        "delete": "Remove a question and its answer. [question #]",
-        "clear": "Clear all question and answers.",
-        "list": "View all questions and their answers.",
-    }
-
     print("======= COMMANDS =======")
     for command, info in commands.items():
         print(f"{command}: {info}")
