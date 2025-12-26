1737d4b75dc7ba80a1388297b8a88668de09f141
droidsareut <droidsareut@proton.me>
2025-12-26 16:36:53 -0500

    Add '.' prefix to keys in commands dictionary

--- commands.py
+++ commands.py
@@ -1,11 +1,11 @@
 question_answers = {}
 commands = {
-    "exit": "Close the Slackuh interface.",
-    "help": "View Slackuh's commands.",
-    "add": "Add a question and its answer. [question] [answer]",
-    "delete": "Remove a question and its answer. [question #]",
-    "clear": "Clear all question and answers.",
-    "list": "View all questions and their answers.",
+    ".exit": "Close the Slackuh interface.",
+    ".help": "View Slackuh's commands.",
+    ".add": "Add a question and its answer. [question] [answer]",
+    ".delete": "Remove a question and its answer. [question #]",
+    ".clear": "Clear all question and answers.",
+    ".list": "View all questions and their answers.",
 }
 
 def list_commands():
