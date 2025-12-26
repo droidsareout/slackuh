49c54e343c48b246372ccd432f1152d87a32f9cf
droidsareut <droidsareut@proton.me>
2025-12-26 17:37:12 -0500

    Change way of finding command doesn't exist

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from commands import list_commands, add_question_answer, command_list
+from commands import list_commands, add_question_answer
 
 running_program = True
 
@@ -7,9 +7,6 @@ print("Slackuh \nType \".help\" for a list of commands.")
 while running_program:
     sent_text = input(">> ")
 
-    if sent_text not in command_list.keys():
-        print(f"\"{sent_text}\" is not a command.")
-
     match sent_text:
         case ".exit":
             running_program = False
@@ -17,3 +14,5 @@ while running_program:
             list_commands()
         case add if sent_text.startswith(".add"):
             add_question_answer(sent_text)
+        case _:
+            print("Error: Command doesn't exist.")
