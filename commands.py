c396bb4d99c99e98b45af85be4e8ba8c2c45e4fa
droidsareut <droidsareut@proton.me>
2025-12-26 18:55:56 -0500

    Change question_answers dictionary to questions_answers

--- commands.py
+++ commands.py
@@ -1,4 +1,4 @@
-question_answers = {}
+questions_answers = {}
 command_list = {
     ".exit": "Close the Slackuh interface.",
     ".help": "View Slackuh's commands.",
@@ -17,20 +17,20 @@ def add_question_answer():
     question = input("Question: ")
     answer = input("Answer: ")
 
-    question_answers[question] = answer
+    questions_answers[question] = answer
 
     print(f"Added question \"{question}\" with answer \"{answer}\".")
 
 def list_questions_answers():
     print("======= QUESTIONS & ANSWERS =======")
-    for index, (key, value) in enumerate(question_answers.items(), 1):
+    for index, (key, value) in enumerate(questions_answers.items(), 1):
         print(f"{index}: {key}\n{value}")
 
 def remove_question_answer():
     question_to_remove = input("Remove question #: ")
     question_to_remove = int(question_to_remove)
 
-    key = list(question_answers.keys())[question_to_remove - 1]
-    del question_answers[key]
+    key = list(questions_answers.keys())[question_to_remove - 1]
+    del questions_answers[key]
 
     print(f"Removed question #{question_to_remove}")
